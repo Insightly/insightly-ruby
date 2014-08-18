92d7d6d87c69d6544e1d8ae75069210593262fe0
Alexander Semyonov <al@semyonov.us>
2014-08-19 03:24:04 +0600

    Provide mocks for JSON responses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+SPEC_ROOT = File.expand_path('../..', __FILE__)
+$LOAD_PATH.unshift File.join(SPEC_ROOT, 'lib')
+require 'webmock/rspec'
 require 'insightly'
+
+RSpec.configure do |config|
+  config.before do
+    stub_request(:any, /#{Regexp.escape('https://api.insight.ly/v2.1/')}(.+)/).to_return(
+      body: lambda { |request|
+        file_name = File.join(SPEC_ROOT, 'spec/fixtures', "#{request.uri.path.gsub('/v2.1/', '').downcase}.json")
+        File.read(file_name)
+      }
+    )
+  end
+end
