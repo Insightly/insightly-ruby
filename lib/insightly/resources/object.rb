6c4f989b4240e963dbab8b78c2689397e0ee86b5
Alexander Semyonov <al@semyonov.us>
2014-08-19 03:53:00 +0600

    Serialize resources for PUT/POST methods

--- lib/insightly/resources/object.rb
+++ lib/insightly/resources/object.rb
@@ -8,7 +8,7 @@ module Insightly
       require 'insightly/resources/object/serializers'
       require 'insightly/resources/object/attributes'
 
-      extend Serializers
+      include Serializers
       include Attributes
 
       # Define common attributes, applicable to different resources
@@ -16,7 +16,13 @@ module Insightly
       attribute :date_updated_utc, Time
 
       def inspect
-        "#<#{self.class.name}:#{object_id} #{attributes.map { |key, value| "@#{key}=#{value.inspect}" }.join(' ')}>"
+        "#<#{self.class.name}:#{'0x00%x' % (object_id << 1)} #{inspect_attributes}>"
+      end
+
+      private
+
+      def inspect_attributes
+        attributes.map { |key, value| "@#{key}=#{value.inspect}" }.join(' ')
       end
     end
   end
--- lib/insightly/resources/object/serializers.rb
+++ lib/insightly/resources/object/serializers.rb
@@ -14,7 +14,7 @@ module Insightly
 
         module Time
           def self.serialize(value)
-            value.xmlschema
+            value.utc.xmlschema
           end
 
           def self.deserialize(value)
@@ -22,48 +22,67 @@ module Insightly
           end
         end
 
-        # @return [Hash] corresponding serializers for different attributes
-        def serializers
-          @serializers ||= {}
-        end
+        module ClassMethods
+          # @return [Hash] corresponding serializers for different attributes
+          def serializers
+            @serializers ||= {}
+          end
 
-        # @param [String, Symbol] type type of attribute to be serialized or deserialized
-        # @return [#serialize, #deserialize] serializer for provided type
-        def serializer_for(type)
-          serializers[type] ||=
-            begin
-              class_symbol = type.to_s.to_sym
-              if type.respond_to?(:deserialize)
-                type
-              elsif Serializers.constants.include?(class_symbol)
-                Serializers.const_get(class_symbol)
-              elsif Resources.constants.include?(class_symbol)
-                Resources.const_get(class_symbol)
-              else
-                Serializers::Object
+          # @param [String, Symbol] type type of attribute to be serialized or deserialized
+          # @return [#serialize, #deserialize] serializer for provided type
+          def serializer_for(type)
+            serializers[type] ||=
+              begin
+                class_symbol = type.to_s.to_sym
+                if type.respond_to?(:deserialize) && type.respond_to?(:serialize)
+                  type
+                elsif Serializers.constants.include?(class_symbol)
+                  Serializers.const_get(class_symbol)
+                elsif Resources.constants.include?(class_symbol)
+                  Resources.const_get(class_symbol)
+                else
+                  Serializers::Object
+                end
               end
+          end
+
+          # @param [Net::HTTPResponse] response
+          # @return [<Object>, Object]
+          def deserialize(response)
+            return nil unless response.is_a?(String) || response.is_a?(Array) || response.is_a?(Net::HTTPOK)
+            if response.respond_to?(:body)
+              response = response.body
+              json = JSON.parse(response)
+            else
+              json = response
             end
-        end
-
-        # @param [Net::HTTPResponse] response
-        # @return [<Object>, Object]
-        def deserialize(response)
-          return nil unless response.is_a?(String) || response.is_a?(Array) || response.is_a?(Net::HTTPOK)
-          if response.respond_to?(:body)
-            response = response.body
-            json = JSON.parse(response)
-          else
-            json = response
+            case json
+            when Array
+              json.map { |object| new(object) }
+            when Hash
+              new(json)
+            end
           end
-          case json
-          when Array
-            json.map { |object| new(object) }
-          when Hash
-            new(json)
+
+          alias_method :parse, :deserialize
+
+          def serialize(object)
+            object.serialize
           end
         end
 
-        alias_method :parse, :deserialize
+        def self.included(base)
+          super
+          base.extend ClassMethods
+        end
+
+        def serialize
+          {}.tap do |result|
+            attributes.each do |name, value|
+              result[name.upcase] = self.class.serializer_for(name).serialize(value)
+            end
+          end
+        end
       end
     end
   end
