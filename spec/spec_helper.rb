57b1b1008536e268a4efd19680b596235ea80a8a
David Iorns <david.iorns@gmail.com>
2014-09-11 10:08:51 -0700

    Better fixture directory structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,13 @@ SPEC_ROOT = File.expand_path('../..', __FILE__)
 $LOAD_PATH.unshift File.join(SPEC_ROOT, 'lib')
 require 'webmock/rspec'
 require 'insightly'
+require 'active_support/inflector'
 
 RSpec.configure do |config|
   config.before do
     stub_request(:any, /#{Regexp.escape('https://api.insight.ly/v2.1/')}(.+)/).to_return(
       body: lambda { |request|
-        file_name = File.join(SPEC_ROOT, 'spec/fixtures', "#{request.uri.path.gsub('/v2.1/', '').downcase}.json")
+        file_name = File.join(SPEC_ROOT, 'spec/fixtures', "#{request.uri.path.gsub('/v2.1/', '').underscore.downcase}.json")
         File.read(file_name)
       }
     )
