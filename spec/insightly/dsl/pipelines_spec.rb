b1aa2bdb3ca01bd9473b09b4e1a42754bac86325
David Iorns <david.iorns@gmail.com>
2014-09-15 15:22:47 -0700

    Add pipeline spec

--- spec/insightly/dsl/pipelines_spec.rb
+++ spec/insightly/dsl/pipelines_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe Insightly::DSL::Pipelines do
   # GET /v2.1/Pipelines
-
+  describe '#get_pipelines' do
+    it 'returns an array of pipelines' do
+      pipelines = Insightly.client.get_pipelines
+      expect(pipelines).to be_a(Array)
+      expect(pipelines.first).to be_a(Insightly::Resources::Pipeline)
+    end
+  end
+  
   # GET /v2.1/Pipelines/{id}
   describe '#get_pipeline' do
     it 'returns a pipeline' do
-      expect(Insightly.client.get_pipeline_stage(1)).to be_a(Insightly::Resources::PipelineStage)
+      expect(Insightly.client.get_pipeline(1)).to be_a(Insightly::Resources::Pipeline)
     end
   end
 end
