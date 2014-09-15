2c03eea21dcd63d8f27bdb6fb826d00c51a6449d
David Iorns <david.iorns@gmail.com>
2014-09-15 15:18:22 -0700

    Add pipeline stages

--- spec/insightly/dsl/pipeline_stages_spec.rb
+++ spec/insightly/dsl/pipeline_stages_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe Insightly::DSL::PipelineStages do
   # GET /v2.1/PipelineStages
-
+  describe '#get_pipeline_stages' do
+    it 'returns an array of pipeline_stages' do
+      pipeline_stages = Insightly.client.get_pipeline_stages
+      expect(pipeline_stages).to be_a(Array)
+      expect(pipeline_stages.first).to be_a(Insightly::Resources::PipelineStage)
+    end
+  end
+  
   # GET /v2.1/PipelineStages/{id}
   describe '#get_pipeline_stage' do
     it 'returns a pipeline stage' do
