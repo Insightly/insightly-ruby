5414ad3b37cab007b7e424abac5065e966c49ce2
Michael Kompanets <mkompanets@gmail.com>
2014-09-23 13:55:12 -0700

    added method scaffolds

--- lib/insightly/dsl/contacts.rb
+++ lib/insightly/dsl/contacts.rb
@@ -2,50 +2,79 @@ require 'insightly/dsl'
 
 module Insightly
   module DSL::Contacts
-    # DELETE /v2.1/Contacts/{c_id}/Image
-
-    # DELETE /v2.1/Contacts/{id}
-    # @param [String, Fixnum] id A Contact's ID
-    def delete_contact(id)
-      request(:delete, "Contacts/#{id}")
+    # GET /v2.1/Contacts?ids={ids}&email={email}&tag={tag}
+    # Gets a list of Contacts.
+    # @param [ids:] Array of contact ids (optional).
+    # @param [email:] email address (optional).
+    # @param [tag:] single tag (optional).
+    def get_contacts(ids: [], email: '', tag: '')
     end
-
-    # GET /v2.1/Contacts/{c_id}/Emails
-
-    # GET /v2.1/Contacts/{c_id}/Image
-
-    # GET /v2.1/Contacts/{c_id}/Notes
-
-    # GET /v2.1/Contacts/{c_id}/Tasks
 
     # GET /v2.1/Contacts/{id}
-    # @return [Insightly::Resources::Contact]
-    # @param [String, Fixnum] id A Contact's ID
-    def get_contact(id)
-      Resources::Contact.parse(request(:get, "Contacts/#{id}"))
-    end
-
-    # GET /v2.1/Contacts?ids={ids}&email={email}&tag={tag}
-    # @param [Hash] options
-    # @option options :ids [Array]
-    # @option options :email [String]
-    # @option options :tag [String]
-    # @return [<Insightly::Resources::Contact>, nil]
-    def get_contacts(options = {})
-      Resources::Contact.parse(request(:get, 'Contacts', options))
+    # Gets a contact by id.
+    # @param [id:] id of the contact.
+    def get_contact(id:)
     end
 
     # POST /v2.1/Contacts
-    # @param [Insightly::Resources::Contact] contact The contact we're creating
-    # @return [<Insightly::Resources::Contact>, nil]
-    def create_contact(contact)
-      Resources::Contact.parse(request(:post, 'Contacts', contact))
+    # Creates a contact.
+    # @param [contact:] Hash of contact attributes.
+    def create_contact(contact:)
+    end
+
+    # PUT /v2.1/Contacts
+    # Updates a contact.
+    # @param [contact:] Hash of contact attributes.
+    def update_contact(contact:)
+    end
+
+    # DELETE /v2.1/Contacts/{id}
+    # Deletes a Contact.
+    # @param [id:] id of the contact.
+    def delete_contact(id:)
+    end
+
+    # GET /v2.1/Contacts/{c_id}/Emails
+    # Gets a Contact's Emails.
+    # @param [id:] id of the contact.
+    def get_contact_emails(id:)
+    end
+
+    # GET /v2.1/Contacts/{c_id}/Notes
+    # Gets a Contact's Notes.
+    # @param [id:] id of the contact.
+    def get_contact_notes(id:)
+    end
+
+    # GET /v2.1/Contacts/{c_id}/Tasks
+    # Gets a Contact's Tasks.
+    # @param [id:] id of the contact.
+    def get_contact_tasks(id:)
+    end
+
+    # GET /v2.1/Contacts/{c_id}/Image
+    # Gets a Contact's Image.
+    # @param [id:] id of the contact.
+    def get_contact_image(id:)
     end
 
     # POST /v2.1/Contacts/{c_id}/Image/{filename}
-
-    # PUT /v2.1/Contacts
+    # Adds a Contact's Image
+    # @param [id:] id of the contact.
+    # @param [filename:] Name of Image File to be attached to Contact.
+    def add_contact_image(id: filename:)
+    end
 
     # PUT /v2.1/Contacts/{c_id}/Image/{filename}
+    # Updates a Contact's Image.
+    # @param [id:] id of the contact.
+    # @param [filename:] Name of Image File to be attached to Contact.
+    def update_contact_image(id: filename:)
+    end
+
+    # DELETE /v2.1/Contacts/{c_id}/Image
+    # Deletes a Contact's Image.
+    def delete_contact_image(id:)
+    end
   end
 end
