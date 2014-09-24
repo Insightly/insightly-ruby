623fe42b31239361b29c6adc2c7c29fae6228034
David Iorns <david.iorns@gmail.com>
2014-09-24 14:23:26 -0700

    Add task categories

--- lib/insightly/dsl/task_categories.rb
+++ lib/insightly/dsl/task_categories.rb
@@ -4,7 +4,7 @@ module Insightly
   module DSL::TaskCategories
     # DELETE /v2.1/TaskCategories/{id}
     # @param [String, Fixnum] id A TaskCategory's ID
-    def delete_task_category(id)
+    def delete_task_category(id:)
       request(:delete, "TaskCategories/#{id}")
     end
 
@@ -17,12 +17,22 @@ module Insightly
     # GET /v2.1/TaskCategories/{id}
     # @return [Insightly::Resources::TaskCategory]
     # @param [String, Fixnum] id A TaskCategory's ID
-    def get_task_category(id)
+    def get_task_category(id:)
       Resources::TaskCategory.parse(request(:get, "TaskCategories/#{id}"))
     end
 
     # POST /v2.1/TaskCategories
+    # Creates a task category.
+    # @param [Hash] task_category A hash of task category attributes.
+    def create_task_category(task_category:)
+      Resources::TaskCategory.parse(request(:post, "TaskCategories", task_category))
+    end
 
     # PUT /v2.1/TaskCategories
+    # Updates a task category.
+    # @param [Hash] task_category A hash of task_category attributes.
+    def update_task_category(task_category:)
+      Resources::TaskCategory.parse(request(:put, "TaskCategories", task_category))
+    end
   end
 end
