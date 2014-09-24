a2097f2a41a41685bdb8cad60717c4f72147ccc5
David Iorns <david.iorns@gmail.com>
2014-09-24 14:15:11 -0700

    Add task post and put

--- lib/insightly/dsl/tasks.rb
+++ lib/insightly/dsl/tasks.rb
@@ -4,31 +4,48 @@ module Insightly
   module DSL::Tasks
     # DELETE /v2.1/Tasks/{id}
     # @param [String, Fixnum] id A Task's ID
-    def delete_task(id)
+    def delete_task(id:)
       request(:delete, "Tasks/#{id}")
     end
 
     # GET /v2.1/Tasks/{c_id}/Comments
+    # @param [String, Fixnum] c_id A Task's ID
+    # @return [Array, nil]
+    def get_task_comments(c_id:)
+      Resources::Task.parse(request(:get, "Tasks/#{c_id}/Comments"))
+    end
 
     # GET /v2.1/Tasks/{id}
-    # @return [Insightly::Resources::Task]
     # @param [String, Fixnum] id A Task's ID
-    def get_task(id)
+    # @return [Insightly::Resources::Task, nil]
+    def get_task(id:)
       Resources::Task.parse(request(:get, "Tasks/#{id}"))
     end
 
     # GET /v2.1/Tasks?ids={ids}
-    # @param [Hash] options
-    # @option options :ids [Array]
+    # @param [ids:] Array of task ids (optional)
     # @return [<Insightly::Resources::Task>, nil]
-    def get_tasks(options = {})
-      Resources::Task.parse(request(:get, 'Tasks', options))
+    def get_tasks(ids: [])
+      Resources::Task.parse(request(:get, "Tasks/?ids=#{ids}"))
     end
 
     # POST /v2.1/Tasks
+    # @param [Hash] task: Hash of task attributes
+    def create_task(task:)
+      Resources::Task.parse(request(:post, "Tasks", task))
+    end
 
     # POST /v2.1/Tasks/{c_id}/Comments
+    # @param [String, Fixnum] c_id A Task's ID
+    # @param [Hash] comment The comment to create
+    def create_task_comments(c_id:, comment:)
+      Resources::Task.parse(request(:post, "Tasks/#{c_id}/Comments", comment))
+    end
 
     # PUT /v2.1/Tasks
+    # @param [Hash] task The task to update
+    def update_task(task:)
+      Resources::Contact.parse(request(:put, "Tasks", task))
+    end
   end
 end
