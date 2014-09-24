594369feb9e82355103d82ccffddc6c5b25fe750
Michael Kompanets <mkompanets@gmail.com>
2014-09-24 15:41:04 -0700

    methods and specs

--- lib/insightly/dsl/opportunity_categories.rb
+++ lib/insightly/dsl/opportunity_categories.rb
@@ -4,7 +4,7 @@ module Insightly
   module DSL::OpportunityCategories
     # DELETE /v2.1/OpportunityCategories/{id}
     # @param [String, Fixnum] id A OpportunityCategory's ID
-    def delete_opportunity_category(id)
+    def delete_opportunity_category(id:)
       request(:delete, "OpportunityCategories/#{id}")
     end
 
@@ -17,12 +17,22 @@ module Insightly
     # GET /v2.1/OpportunityCategories/{id}
     # @return [Insightly::Resources::OpportunityCategory]
     # @param [String, Fixnum] id A OpportunityCategory's ID
-    def get_opportunity_category(id)
+    def get_opportunity_category(id:)
       Resources::OpportunityCategory.parse(request(:get, "OpportunityCategories/#{id}"))
     end
 
     # POST /v2.1/OpportunityCategories
+    # @param [Hash] category A Hash of category attributes.
+    # @return [Insightly::Resources::OpportunityCategory]
+    def create_opportunity_category(category:)
+      Resources::OpportunityCategory.parse(request(:post, "OpportunityCategories", category))
+    end
 
     # PUT /v2.1/OpportunityCategories
+    # @param [Hash] category A Hash of category attributes.
+    # @return [Insightly::Resources::OpportunityCategory]
+    def update_opportunity_category(category:)
+      Resources::OpportunityCategory.parse(request(:put, "OpportunityCategories", category))
+    end
   end
 end
