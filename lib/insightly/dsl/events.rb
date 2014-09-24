dbb12e4c68cfe325ba98cc9e8c1325f87a69647d
Michael Kompanets <mkompanets@gmail.com>
2014-09-24 11:31:05 -0700

    added spec stubs

--- lib/insightly/dsl/events.rb
+++ lib/insightly/dsl/events.rb
@@ -3,7 +3,7 @@ require 'insightly/dsl'
 module Insightly
   module DSL::Events
     # DELETE /v2.1/Events/{id}
-    # @param [String, Fixnum] id A Event's ID
+    # @param [String, Fixnum] id: id A Event's ID
     def delete_event(id:)
       request(:delete, "Events/#{id}")
     end
@@ -16,13 +16,23 @@ module Insightly
 
     # GET /v2.1/Events/{id}
     # @return [Insightly::Resources::Event]
-    # @param [String, Fixnum] id A Event's ID
+    # @param [String, Fixnum] id: id A Event's ID
     def get_event(id:)
       Resources::Event.parse(request(:get, "Events/#{id}"))
     end
 
     # POST /v2.1/Events
+    # @param [Hash] event: event attributes.
+    # @return [<Insightly::Resources::Event>]
+    def create_event(event:)
+       Resources::Event.parse(request(:post, "Events", event))
+    end
 
     # PUT /v2.1/Events
+    # @param [Hash] event: event attributes.
+    # @return [<Insightly::Resources::Event>]
+    def update_event(event:)
+       Resources::Event.parse(request(:put, "Events", event))
+    end
   end
 end
