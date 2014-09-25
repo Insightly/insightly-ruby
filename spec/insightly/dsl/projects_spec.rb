075ffddca8773615982683e7353e005409d408bb
Michael Kompanets <mkompanets@gmail.com>
2014-09-24 17:46:24 -0700

    methods and specs

--- spec/insightly/dsl/projects_spec.rb
+++ spec/insightly/dsl/projects_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Insightly::DSL::Projects do
   # DELETE /v2.1/Projects/{c_id}/Image
+  describe '#delete_project_image' do
+    it 'returns a response with code 202' do
+      response = Insightly.client.delete_project_image(id: 1)
+      expect(response.code).to eq(202)
+    end
+  end
 
   # DELETE /v2.1/Projects/{id}
   describe '#delete_project' do
@@ -12,12 +18,40 @@ describe Insightly::DSL::Projects do
   end
 
   # GET /v2.1/Projects/{c_id}/Emails
+  describe '#get_project_emails' do
+    it 'returns project emails' do
+      response = Insightly.client.get_project_emails(id: 1)
+      expect(response).to be_a(Array)
+      expect(response.first).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # GET /v2.1/Projects/{c_id}/Image
+  describe '#get_project_image' do
+    it 'returns project image' do
+      response = Insightly.client.get_project_image(id: 1)
+      expect(response).to be_a(Array)
+      expect(response.first).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # GET /v2.1/Projects/{c_id}/Notes
+  describe '#get_project_notes' do
+    it 'returns project notes' do
+      response = Insightly.client.get_project_notes(id: 1)
+      expect(response).to be_a(Array)
+      expect(response.first).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # GET /v2.1/Projects/{c_id}/Tasks
+  describe '#get_project_tasks' do
+    it 'returns project tasks' do
+      response = Insightly.client.get_project_tasks(id: 1)
+      expect(response).to be_a(Array)
+      expect(response.first).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # GET /v2.1/Projects/{id}
   describe '#get_project' do
@@ -36,10 +70,30 @@ describe Insightly::DSL::Projects do
   end
 
   # POST /v2.1/Projects
+  describe '#create_project' do
+    it 'creates and returns a project' do
+      expect(Insightly.client.create_project(project: {})).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # POST /v2.1/Projects/{c_id}/Image/{filename}
+  describe '#create_project_image' do
+    xit 'returns 201' do
+      response = Insightly.client.create_project_image(id: 1, filename: '')
+    end
+  end
 
   # PUT /v2.1/Projects
+  describe '#update_project' do
+    it 'updates and returns a project' do
+      expect(Insightly.client.update_project(project: {})).to be_a(Insightly::Resources::Project)
+    end
+  end
 
   # PUT /v2.1/Projects/{c_id}/Image/{filename}
+  describe '#update_project_image' do
+    xit 'returns 201' do
+      response = Insightly.client.update_project_image(id: 1, filename: '')
+    end
+  end
 end
