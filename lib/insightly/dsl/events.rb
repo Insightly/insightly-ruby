2980eced5dae6d7c32d0507bf2adb589ef13fc6f
David Iorns <david.iorns@gmail.com>
2014-09-29 16:59:40 -0700

    Add events args checking

--- lib/insightly/dsl/events.rb
+++ lib/insightly/dsl/events.rb
@@ -5,8 +5,10 @@ module Insightly
     # GET /v2.1/Events/{id}
     # Get an event.
     # @param [String, Fixnum] id An event's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Event, nil].
     def get_event(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       Resources::Event.parse(request(:get, "Events/#{id}"))
     end
 
@@ -20,24 +22,30 @@ module Insightly
     # POST /v2.1/Events
     # Create an event.
     # @param [Hash] event The event to create.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Event, nil]
     def create_event(event:)
-       Resources::Event.parse(request(:post, "Events", event))
+      raise ArgumentError, "Event cannot be nil" if event.nil?
+      Resources::Event.parse(request(:post, "Events", event))
     end
 
     # PUT /v2.1/Events
     # Update an event.
     # @param [Hash] event The event to update.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Event, nil]
     def update_event(event:)
-       Resources::Event.parse(request(:put, "Events", event))
+      raise ArgumentError, "Event cannot be nil" if event.nil?
+      Resources::Event.parse(request(:put, "Events", event))
     end
 
     # DELETE /v2.1/Events/{id}
     # Delete an event.
     # @param [String, Fixnum] id An event's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [RestClient::Response].
     def delete_event(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       request(:delete, "Events/#{id}")
     end
   end
