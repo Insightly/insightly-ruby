f512e8c7d51e67fe5a7c82ec5d40a64f88c98452
David Iorns <david.iorns@gmail.com>
2014-09-29 17:07:20 -0700

    Add notes args checking

--- lib/insightly/dsl/notes.rb
+++ lib/insightly/dsl/notes.rb
@@ -5,16 +5,20 @@ module Insightly
     # GET /v2.1/Notes/{id}
     # Get a note.
     # @param [String, Fixnum] id A note's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Note, nil].
     def get_note(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       Resources::Note.parse(request(:get, "Notes/#{id}"))
     end
 
     # GET /v2.1/Notes/{c_id}/Comments
     # Gets the comments attached to a note.
     # @param [String, Fixnum] id A note's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Array, nil].
     def get_note_comments(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       Resources::Comment.parse(request(:get, "Notes/#{id}/Comments"))
     end
 
@@ -28,8 +32,10 @@ module Insightly
     # POST /v2.1/Notes
     # Creates a note.
     # @param [Hash] note The note to create.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Note, nil].
     def create_note(note:)
+      raise ArgumentError, "Note cannot be nil" if note.nil?
       Resources::Note.parse(request(:post, "Notes", note))
     end
 
@@ -41,22 +47,30 @@ module Insightly
     # Adds a File Attachment to a Note.
     # @param [String|Fixnum] id A Note's ID.
     # @param [String] filename The name of the file.
+    # @raise [ArgumentError] If the method arguments are nil.
+    # @return [RestClient::Response].
     def create_note_file(id:, filename:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
+      raise ArgumentError, "Filename cannot be nil" if filename.nil?
       request(:post, "Notes?c_id=#{id}&filename=#{filename}")
     end
 
     # PUT /v2.1/Notes
     # Updates a note.
     # @param [Hash] note The note to update.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Note, nil].
     def update_note(note:)
+      raise ArgumentError, "Note cannot be nil" if note.nil?
       Resources::Note.parse(request(:put, "Notes", note))
     end
 
     # DELETE /v2.1/Notes/{id}
     # @param [String, Fixnum] id A note's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [RestClient::Response].
     def delete_note(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       request(:delete, "Notes/#{id}")
     end
   end
