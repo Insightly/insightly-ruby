42e14c093dc11c0cddb20025f68e075bb56d2032
David Iorns <david.iorns@gmail.com>
2014-09-29 17:58:33 -0700

    Add comment args checking

--- lib/insightly/dsl/comments.rb
+++ lib/insightly/dsl/comments.rb
@@ -5,8 +5,10 @@ module Insightly
     # GET /v2.1/Comments/{id}
     # Get a comment.
     # @param [String, Fixnum] id A comment's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Comment, nil].
     def get_comment(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       Resources::Comment.parse(request(:get, "Comments/#{id}"))
     end
 
@@ -14,23 +16,31 @@ module Insightly
     # Adds a file attachment to a comment.
     # @param [String, Fixnum] id A comment's ID.
     # @param [String] filename The name of the attachment.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [RestClient::Response].
     def create_comment_attachment(id:, filename:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
+      raise ArgumentError, "Filename cannot be nil" if filename.nil?
       request(:post, "Comments/?c_id=#{id}&filename=#{filename}")
     end
 
     # PUT /v2.1/Comments
-    # Updates a Comment.
+    # Updates a comment.
     # @param [Hash] comment The comment to update.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Comment, nil].
     def update_comment(comment:)
+      raise ArgumentError, "Comment cannot be nil" if comment.nil?
       Resources::Comment.parse(request(:put, "Comments", comment))
     end
 
     # DELETE /v2.1/Comments/{id}
+    # Deletes a comment.
     # @param [String, Fixnum] id A comment's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [RestClient::Response].
     def delete_comment(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       request(:delete, "Comments/#{id}")
     end
   end
