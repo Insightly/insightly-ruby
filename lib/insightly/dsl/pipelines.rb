247ad0c46e29ac9cc1969ab53778b68cfb105ea8
David Iorns <david.iorns@gmail.com>
2014-09-29 17:18:38 -0700

    Add pipelines args checking

--- lib/insightly/dsl/pipelines.rb
+++ lib/insightly/dsl/pipelines.rb
@@ -5,8 +5,10 @@ module Insightly
     # GET /v2.1/Pipelines/{id}
     # Get a pipeline.
     # @param [String, Fixnum] id A pipeline's ID.
+    # @raise [ArgumentError] If the method arguments are nil.
     # @return [Insightly::Resources::Pipeline, nil].
     def get_pipeline(id:)
+      raise ArgumentError, "ID cannot be nil" if id.nil?
       Resources::Pipeline.parse(request(:get, "Pipelines/#{id}"))
     end
 
@@ -14,7 +16,7 @@ module Insightly
     # Get a list of pipelines.
     # @return [Insightly::Resources::Pipeline, nil].
     def get_pipelines
-      Resources::Pipeline.parse(request(:get, 'Pipelines'))
+      Resources::Pipeline.parse(request(:get, "Pipelines"))
     end
   end
 end
