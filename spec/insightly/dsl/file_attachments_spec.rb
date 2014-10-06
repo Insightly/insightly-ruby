436ffeeebc8c0510131b65305eba24f076e8348a
David Iorns <david.iorns@gmail.com>
2014-10-06 11:04:03 -0700

    Fix comment

--- spec/insightly/dsl/file_attachments_spec.rb
+++ spec/insightly/dsl/file_attachments_spec.rb
@@ -5,7 +5,7 @@ describe Insightly::DSL::FileAttachments do
 
   # GET /v2.1/FileAttachments/{id}
   describe '#get_file_attachment' do
-    it 'returns a response with code 202' do
+    it 'returns a response with code 200' do
       VCR.use_cassette('get_file_attachment') do
         response = Insightly.client.get_file_attachment(id: file_attachment_id)
         expect(response.status).to eq(200)
